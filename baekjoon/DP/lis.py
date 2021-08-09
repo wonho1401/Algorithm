76f1af66781dbfb2bbc821822f783eaba342be55
wonho <59119468+wonho1401@users.noreply.github.com>
2021-08-09 14:55:30 +0900

    11053

---
+++ baekjoon/DP/lis.py
@@ -0,0 +1,8 @@
+n = int(input())
+li = list(map(int, input().split()))
+answer = [1]*n
+for i in range(n):
+    for j in range(i):
+        if li[i] > li[j]:
+            answer[i] = max(answer[i],answer[j]+1)
+print(max(answer))

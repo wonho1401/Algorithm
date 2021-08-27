1599ca1616959cbd35c19f476aa2c39c68ea682a
wonho <59119468+wonho1401@users.noreply.github.com>
2021-08-27 11:09:22 +0900

    11052

---
+++ baekjoon/DP/카드구매하기.py
@@ -0,0 +1,9 @@
+n = int(input())
+card = [0]
+card += list(map(int, input().split()))
+
+dp = [0]*(n+1)
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        dp[i] = max(dp[i], dp[i-j]+card[j])
+print(dp[n])
